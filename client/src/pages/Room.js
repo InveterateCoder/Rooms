1a3a96f6e3088e4d2b874f1e9d54b12436708ef2
InveterateCoder <inveterate.coder@gmail.com>
2020-03-18 18:09:13 +0300

    hello world

--- client/src/pages/Room.js
+++ client/src/pages/Room.js
@@ -5,7 +5,7 @@ import { Menu } from "./accessories/Room/Menu";
 import { Toast } from "react-bootstrap";
 import Flag from "react-flags";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight, faInfoCircle, faExclamationTriangle, faSignInAlt, faArrowCircleDown } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight, faInfoCircle, faExclamationTriangle, faSignInAlt, faArrowCircleDown, faPaperPlane } from "@fortawesome/free-solid-svg-icons";
 import LocalizedStrings from "react-localization";
 import Password from "react-type-password";
 import validator from "../utils/validator";
@@ -54,7 +54,7 @@ const text = new LocalizedStrings({
     }
 })
 const pub = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="globe" class="svg-inline--fa fa-globe fa-w-16 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" color="#007bff"><path fill="currentColor" d="M336.5 160C322 70.7 287.8 8 248 8s-74 62.7-88.5 152h177zM152 256c0 22.2 1.2 43.5 3.3 64h185.3c2.1-20.5 3.3-41.8 3.3-64s-1.2-43.5-3.3-64H155.3c-2.1 20.5-3.3 41.8-3.3 64zm324.7-96c-28.6-67.9-86.5-120.4-158-141.6 24.4 33.8 41.2 84.7 50 141.6h108zM177.2 18.4C105.8 39.6 47.8 92.1 19.3 160h108c8.7-56.9 25.5-107.8 49.9-141.6zM487.4 192H372.7c2.1 21 3.3 42.5 3.3 64s-1.2 43-3.3 64h114.6c5.5-20.5 8.6-41.8 8.6-64s-3.1-43.5-8.5-64zM120 256c0-21.5 1.2-43 3.3-64H8.6C3.2 212.5 0 233.8 0 256s3.2 43.5 8.6 64h114.6c-2-21-3.2-42.5-3.2-64zm39.5 96c14.5 89.3 48.7 152 88.5 152s74-62.7 88.5-152h-177zm159.3 141.6c71.4-21.2 129.4-73.7 158-141.6h-108c-8.8 56.9-25.6 107.8-50 141.6zM19.3 352c28.6 67.9 86.5 120.4 158 141.6-24.4-33.8-41.2-84.7-50-141.6h-108z"></path></svg>';
-const sec = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user-friends" class="svg-inline--fa fa-user-friends fa-w-20 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" color="#17a2b8"><path fill="currentColor" d="M192 256c61.9 0 112-50.1 112-112S253.9 32 192 32 80 82.1 80 144s50.1 112 112 112zm76.8 32h-8.3c-20.8 10-43.9 16-68.5 16s-47.6-6-68.5-16h-8.3C51.6 288 0 339.6 0 403.2V432c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48v-28.8c0-63.6-51.6-115.2-115.2-115.2zM480 256c53 0 96-43 96-96s-43-96-96-96-96 43-96 96 43 96 96 96zm48 32h-3.8c-13.9 4.8-28.6 8-44.2 8s-30.3-3.2-44.2-8H432c-20.4 0-39.2 5.9-55.7 15.4 24.4 26.3 39.7 61.2 39.7 99.8v38.4c0 2.2-.5 4.3-.6 6.4H592c26.5 0 48-21.5 48-48 0-61.9-50.1-112-112-112z"></path></svg>';
+const sec = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="user-friends" class="svg-inline--fa fa-user-friends fa-w-20 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" color="#343a40"><path fill="currentColor" d="M192 256c61.9 0 112-50.1 112-112S253.9 32 192 32 80 82.1 80 144s50.1 112 112 112zm76.8 32h-8.3c-20.8 10-43.9 16-68.5 16s-47.6-6-68.5-16h-8.3C51.6 288 0 339.6 0 403.2V432c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48v-28.8c0-63.6-51.6-115.2-115.2-115.2zM480 256c53 0 96-43 96-96s-43-96-96-96-96 43-96 96 43 96 96 96zm48 32h-3.8c-13.9 4.8-28.6 8-44.2 8s-30.3-3.2-44.2-8H432c-20.4 0-39.2 5.9-55.7 15.4 24.4 26.3 39.7 61.2 39.7 99.8v38.4c0 2.2-.5 4.3-.6 6.4H592c26.5 0 48-21.5 48-48 0-61.9-50.1-112-112-112z"></path></svg>';
 export class Room extends Component {
     static contextType = Context;
     constructor(props, context) {
@@ -75,7 +75,7 @@ export class Room extends Component {
             public: true,
             selusers: [],
             toasts: [],
-            sound: null,
+            sound: false,
             scrolledDown: true
         }
         this.msgsCount = 50;
@@ -94,7 +94,12 @@ export class Room extends Component {
         this.menu = React.createRef();
         this.msgpanel = React.createRef();
         this.toastsRef = React.createRef();
+        this.scrDownBtnRef = React.createRef();
+        this.inputRef = React.createRef();
         this.toastTimer = null;
+        this.soundMsg = new Audio(window.location.origin + "/msg.ogg");
+        this.soundNotif = new Audio(window.location.origin + "/notif.ogg");
+        this.inputHeight = 41;
     }
     openmenu = () => {
         this.menu.current.focus();
@@ -139,9 +144,9 @@ export class Room extends Component {
     }
     soundClicked = () => {
         if (this.state.sound)
-            this.setState({ sound: null });
+            this.setState({ sound: false });
         else
-            this.setState({ sound: new Audio(window.location.origin + "/msg.mp3") });
+            this.setState({ sound: true });
     }
     passwordChanged = val => this.setState({ password: val });
     passwordKeyPressed = ev => {
@@ -207,7 +212,7 @@ export class Room extends Component {
             </Toast>);
         clearTimeout(this.toastTimer);
         this.toastTimer = setTimeout(() => {
-            this.toastsRef.current.scrollTo(0, this.toastsRef.current.scrollHeight);
+            this.toastsRef.current.scrollTo(0, 0);
         }, 200);
         return toasts;
     }
@@ -231,8 +236,8 @@ export class Room extends Component {
         elem.className = "media p-3 mb-3";
         elem.innerHTML = `<img src="/img/${msg.icon}.m.svg" alt="icon" class="mr-3" />
         <div class="media-body">
-        <h5 class="text-secondary">${msg.secret ? sec : pub}${msg.sender}<small class="ml-2">${time ? "<code>" + time + "</code>" : "&#8987;"}</small></h5>
-        <p class="mb-0">${msg.text}</p>
+        <h5 class="text-info">${msg.secret ? sec : pub}${msg.sender}<small class="ml-2">${time ? "<code>" + time + "</code>" : "&#8987;"}</small></h5>
+        <pre>${msg.text}</pre>
         </div>`;
         return elem;
     }
@@ -241,7 +246,8 @@ export class Room extends Component {
         let date = new Date(id);
         let time = `${date.getHours()}:${date.getMinutes()}`;
         let timeoutId = setTimeout(() => this.removeNotification(id), 7000);
-        this.setState({ toasts: [...this.state.toasts, [id, time, msg, timeoutId]] });
+        this.setState({ toasts: [[id, time, msg, timeoutId], ...this.state.toasts] },
+            () => this.state.sound && this.soundNotif.play());
     }
     removeNotification = id => {
         let notif = this.state.toasts.find(el => el[0] === id);
@@ -292,12 +298,12 @@ export class Room extends Component {
         }).catch(err => this.setState({ failed: err.message || text.wrong }));
     }
     msgInputKeyPressed = ev => {
-        if (ev.which === 13)
-            this.sendMsg(ev.target);
+        /*if (ev.which === 13)
+            this.sendMsg(ev.target);*/
     }
     recieveMessage = msg => {
         this.appendMessage(this.formMessage(msg, new Date()));
-        if (this.state.sound) this.state.sound.play();
+        if (this.state.sound) this.soundMsg.play();
         if (this.state.scrolledDown) document.scrollingElement.scrollTo(0, document.scrollingElement.scrollHeight);
     }
     roomDeleted = () => {
@@ -384,15 +390,20 @@ export class Room extends Component {
             <div ref={this.toastsRef}>
                 {this.fillToasts()}
             </div>
-            {!this.state.scrolledDown && <button id="scrollDown" className="btn btn-dark"
-                onClick={() => document.scrollingElement.scrollTo(0, document.scrollingElement.scrollHeight)}><FontAwesomeIcon icon={faArrowCircleDown} /></button>}
+            <button id="scrollDown" style={{ visibility: this.state.scrolledDown ? "hidden" : "visible" }} ref={this.scrDownBtnRef} className="btn btn-outline-primary"
+                onClick={() => document.scrollingElement.scrollTo(0, document.scrollingElement.scrollHeight)}><FontAwesomeIcon icon={faArrowCircleDown} /></button>
             <div id="roomcont" className="container-fluid">
                 <nav className="navbar navbar-expand bg-dark navbar-dark">
                     <button onClick={this.openmenu} className="btnmenu btn btn-dark mr-3"><FontAwesomeIcon icon={faArrowRight} /></button>
                     <Flag className="mr-3" name={this.state.flag} format="png" pngSize={24} shiny={true} basePath="/img" />
                     <span className="navbar-brand">{this.state.roomname}</span>
                 </nav>
-                <input id="input" type="text" onKeyPress={this.msgInputKeyPressed} className="form-control" placeholder={text.placeholder} />
+                <div id="inpgroup" className="input-group">
+                    <textarea id="input" ref={this.inputRef} onKeyPress={this.msgInputKeyPressed} className="form-control" placeholder={text.placeholder} />
+                    <div className="input-group-append">
+                        <button className="btn btn-outline-secondary ml-2"><FontAwesomeIcon icon={faPaperPlane} /></button>
+                    </div>
+                </div>
                 <div ref={this.msgpanel} id="msgpanel"></div>
             </div>
             <Menu registered={this.context.registered} lang={this.context.lang} menu={this.menu} open={this.state.menuopen}
@@ -401,7 +412,39 @@ export class Room extends Component {
                 setPublic={this.setPublic} sound={this.state.sound} soundClicked={this.soundClicked} />
         </div>
     }
+    inputChanged = () => {
+        let value = this.inputRef.current.value;
+        if (value[value.length - 1] === '\n' && (value[value.length - 2] === '\n' ||
+            (value.match(new RegExp('\n', 'g') || [])).length > 11))
+            this.inputRef.current.value = value.substring(0, value.length - 1);
+        else {
+            this.inputRef.current.style.height = "36px";
+            this.inputRef.current.style.height = this.inputRef.current.scrollHeight + 5 + "px";
+            let top = this.inputRef.current.scrollTop + this.inputRef.current.offsetHeight;
+            if (top - 2 !== this.inputRef.current.scrollHeight && this.inputRef.current.scrollHeight - top < 36)
+                this.inputRef.current.scrollTo(0, this.inputRef.current.scrollHeight);
+            let newHeigh = this.inputRef.current.offsetHeight;
+            if (this.inputMaxed && newHeigh < 161) this.inputMaxed = false;
+            if (!this.inputMaxed && newHeigh !== this.inputHeight) {
+                if (newHeigh === 161) this.inputMaxed = true;
+                this.inputResized(newHeigh - this.inputHeight);
+                this.inputHeight = newHeigh;
+            }
+        }
+    }
+    inputResized = diff => {
+        this.msgpanel.current.style.marginBottom = (parseInt(this.msgpanel.current.style.marginBottom) || 15) + diff + "px";
+        if (this.state.scrolledDown)
+            document.scrollingElement.scrollTo(0, document.scrollingElement.scrollHeight);
+        else
+            document.scrollingElement.scrollTo(0, document.scrollingElement.scrollTop + diff);
+        let style = getComputedStyle(this.scrDownBtnRef.current);
+        this.scrDownBtnRef.current.style.bottom = parseInt(style.bottom) + diff + "px";
+        style = getComputedStyle(this.toastsRef.current);
+        this.toastsRef.current.style.bottom = parseInt(style.bottom) + diff + "px";
+    }
     async componentDidMount() {
+        setTimeout(() => this.inputRef.current.addEventListener("input", this.inputChanged), 500);
         window.addEventListener("scroll", this.windowScrolled);
         try {
             await this.connection.start();
@@ -414,6 +457,7 @@ export class Room extends Component {
         }
     }
     componentWillUnmount() {
+        this.inputRef.current.removeEventListener("input", this.inputChanged);
         window.removeEventListener("scroll", this.windowScrolled);
         this.connection.stop();
     }
