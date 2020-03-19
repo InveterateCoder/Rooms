377e211e2ad8f257ee4faaeb02b5cfa792a13234
InveterateCoder <inveterate.coder@gmail.com>
2020-03-19 12:36:14 +0300

    good morning

--- client/src/pages/Room.js
+++ client/src/pages/Room.js
@@ -11,6 +11,15 @@ import Password from "react-type-password";
 import validator from "../utils/validator";
 import * as signalR from "@aspnet/signalr";
 
+function isMobileTablet() {
+    var check = false;
+    (function (a) {
+        if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4)))
+            check = true;
+    })(navigator.userAgent || navigator.vendor || window.opera);
+    return check;
+}
+
 const text = new LocalizedStrings({
     en: {
         placeholder: "Type a message...",
@@ -78,7 +87,8 @@ export class Room extends Component {
             selusers: [],
             toasts: [],
             sound: false,
-            scrolledDown: true
+            scrolledDown: true,
+            inputFocused: false
         }
         this.msgsCount = 50;
         this.oldestMsgTime = null;
@@ -102,6 +112,7 @@ export class Room extends Component {
         this.soundMsg = new Audio(window.location.origin + "/msg.ogg");
         this.soundNotif = new Audio(window.location.origin + "/notif.ogg");
         this.inputHeight = 41;
+        this.isMobile = isMobileTablet();
     }
     openmenu = () => {
         this.menu.current.focus();
@@ -213,7 +224,17 @@ export class Room extends Component {
                 <Toast.Body>{msg}</Toast.Body>
             </Toast>);
         clearTimeout(this.toastTimer);
-        this.toastTimer = setTimeout(() => this.toastsRef.current.scrollTo(0, 0), 200);
+        this.toastTimer = setTimeout(() => {
+            this.toastsRef.current.scrollTo(0, 0);
+            let heightBoundry = document.scrollingElement.clientHeight - 300
+            if (!this.toastsMaxSet && this.toastsRef.current.scrollHeight > heightBoundry) {
+                this.toastsMaxSet = true;
+                this.toastsRef.current.style.maxHeight = heightBoundry + 147 + "px";
+            } else if (this.toastsMaxSet && this.toastsRef.current.scrollHeight <= heightBoundry) {
+                this.toastsMaxSet = false;
+                this.toastsRef.current.style.bottom = "";
+            }
+        }, 300);
         return toasts;
     }
     formTime = (ticks, today) => {
@@ -304,7 +325,7 @@ export class Room extends Component {
     }
     msgInputKeyPressed = ev => {
         if (ev.which === 13) {
-            if (ev.shiftKey) {
+            if (!this.isMobile && !ev.shiftKey) {
                 ev.preventDefault();
                 this.sendMsg();
             } else if (ev.target.value[ev.target.value.length - 1] === '\n')
@@ -378,8 +399,8 @@ export class Room extends Component {
         });
     }
     inputChanged = () => {
-        this.inputRef.current.style.height = "36px";
-        this.inputRef.current.style.height = this.inputRef.current.scrollHeight + 5 + "px";
+        this.inputRef.current.style.height = "38px";
+        this.inputRef.current.style.height = this.inputRef.current.scrollHeight + 2 + "px";
         let top = this.inputRef.current.scrollTop + this.inputRef.current.offsetHeight;
         if (top - 2 !== this.inputRef.current.scrollHeight && this.inputRef.current.scrollHeight - top < 36)
             this.inputRef.current.scrollTo(0, this.inputRef.current.scrollHeight);
@@ -402,6 +423,11 @@ export class Room extends Component {
         style = getComputedStyle(this.toastsRef.current);
         this.toastsRef.current.style.bottom = parseInt(style.bottom) + diff + "px";
     }
+    inputBlur = ev => {
+        if (ev.relatedTarget && ev.relatedTarget.id === "sendBtn")
+            ev.target.focus();
+        else this.setState({ inputFocused: false });
+    }
     render() {
         text.setLanguage(this.context.lang);
         if (this.state.failed || this.state.warning) return <div id="failed">
@@ -422,21 +448,23 @@ export class Room extends Component {
             </div>
         </div>
         else return <div id="room">
-            <div id="toasts" ref={this.toastsRef}>
+            <div id="toasts" ref={this.toastsRef} style={{ visibility: this.state.toasts.length ? "visible" : "hidden" }}>
                 {this.fillToasts()}
             </div>
             <button id="scrollDown" style={{ visibility: this.state.scrolledDown ? "hidden" : "visible" }} ref={this.scrDownBtnRef} className="btn btn-primary"
-                onClick={() => document.scrollingElement.scrollTo(0, document.scrollingElement.scrollHeight)}><FontAwesomeIcon icon={faArrowCircleDown} /></button>
+                onClick={() => document.scrollingElement.scrollTo({ top: document.scrollingElement.scrollHeight, left: 0, behavior: "smooth" })}><FontAwesomeIcon icon={faArrowCircleDown} /></button>
             <div id="roomcont" className="container-fluid">
                 <nav className="navbar navbar-expand bg-dark navbar-dark">
                     <button onClick={this.openmenu} className="btnmenu btn btn-dark mr-3"><FontAwesomeIcon icon={faArrowRight} /></button>
                     <Flag className="mr-3" name={this.state.flag} format="png" pngSize={24} shiny={true} basePath="/img" />
                     <span className="navbar-brand">{this.state.roomname}</span>
                 </nav>
-                <div id="inpgroup" className="input-group">
-                    <textarea id="input" ref={this.inputRef} onInput={this.inputChanged} onKeyPress={this.msgInputKeyPressed} className="form-control" placeholder={text.placeholder} />
+                <div id="inpgroup" className={`input-group${this.state.inputFocused ? " focused" : ""}`}>
+                    <textarea id="input" ref={this.inputRef} onFocus={() => this.setState({ inputFocused: true })}
+                        onInput={this.inputChanged} onKeyPress={this.msgInputKeyPressed} className="form-control"
+                        placeholder={text.placeholder} onBlur={this.inputBlur} />
                     <div className="input-group-append">
-                        <button className="btn btn-outline-secondary ml-2" onClick={this.sendMsg}><FontAwesomeIcon icon={faPaperPlane} /></button>
+                        <button id="sendBtn" className={`btn btn-success ml-${this.state.inputFocused ? "1 mr-1" : "2"}`} onClick={this.sendMsg}><FontAwesomeIcon icon={faPaperPlane} /></button>
                     </div>
                 </div>
                 <div ref={this.msgpanel} id="msgpanel"></div>
@@ -447,8 +475,14 @@ export class Room extends Component {
                 setPublic={this.setPublic} sound={this.state.sound} soundClicked={this.soundClicked} />
         </div>
     }
+    windowResized = () => {
+        if (this.state.scrolledDown)
+            document.scrollingElement.scrollTo(0, document.scrollingElement.scrollHeight);
+        this.inputChanged();
+    }
     async componentDidMount() {
         window.addEventListener("scroll", this.windowScrolled);
+        window.addEventListener("resize", this.windowResized);
         try {
             await this.connection.start();
             let data = await this.connection.invoke("Enter", this.props.match.params["room"],
@@ -461,6 +495,7 @@ export class Room extends Component {
     }
     componentWillUnmount() {
         window.removeEventListener("scroll", this.windowScrolled);
+        window.removeEventListener("resize", this.windowResized);
         this.connection.stop();
     }
 }
