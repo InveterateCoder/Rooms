58c4b8e973314f277c0cbff9bc08c19f23e0e045
InveterateCoder <inveterate.coder@gmail.com>
2020-03-27 17:34:14 +0300

    fixing bugs

--- client/src/pages/Room.js
+++ client/src/pages/Room.js
@@ -42,7 +42,9 @@ const text = new LocalizedStrings({
         changedIcon: "has changed icon.",
         roomnameChanged: "The room's name has changed.",
         flagChanged: "The room's flag has changed.",
-        bothChanged: "The room's name and flag have changed."
+        bothChanged: "The room's name and flag have changed.",
+        rmspassword: "Room's password",
+        pswdplcholder: "Enter password"
     },
     ru: {
         placeholder: "Введите сообщение ...",
@@ -63,7 +65,9 @@ const text = new LocalizedStrings({
         changedIcon: "изменил значок.",
         roomnameChanged: "Название комнаты изменилось.",
         flagChanged: "Флаг комнаты изменился.",
-        bothChanged: "Название комнаты и флаг изменились."
+        bothChanged: "Название комнаты и флаг изменились.",
+        rmspassword: "Пароль комнаты",
+        pswdplcholder: "Введите пароль"
     }
 })
 const pub = '<svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="globe" class="svg-inline--fa fa-globe fa-w-16 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" color="#007bff"><path fill="currentColor" d="M336.5 160C322 70.7 287.8 8 248 8s-74 62.7-88.5 152h177zM152 256c0 22.2 1.2 43.5 3.3 64h185.3c2.1-20.5 3.3-41.8 3.3-64s-1.2-43.5-3.3-64H155.3c-2.1 20.5-3.3 41.8-3.3 64zm324.7-96c-28.6-67.9-86.5-120.4-158-141.6 24.4 33.8 41.2 84.7 50 141.6h108zM177.2 18.4C105.8 39.6 47.8 92.1 19.3 160h108c8.7-56.9 25.5-107.8 49.9-141.6zM487.4 192H372.7c2.1 21 3.3 42.5 3.3 64s-1.2 43-3.3 64h114.6c5.5-20.5 8.6-41.8 8.6-64s-3.1-43.5-8.5-64zM120 256c0-21.5 1.2-43 3.3-64H8.6C3.2 212.5 0 233.8 0 256s3.2 43.5 8.6 64h114.6c-2-21-3.2-42.5-3.2-64zm39.5 96c14.5 89.3 48.7 152 88.5 152s74-62.7 88.5-152h-177zm159.3 141.6c71.4-21.2 129.4-73.7 158-141.6h-108c-8.8 56.9-25.6 107.8-50 141.6zM19.3 352c28.6 67.9 86.5 120.4 158 141.6-24.4-33.8-41.2-84.7-50-141.6h-108z"></path></svg>';
@@ -450,9 +454,9 @@ export class Room extends Component {
         </div>
         else if (this.state.loading) return <Preloader />
         else if (this.state.blocked) return <div id="roompass">
-            <h4 className="text-info mb-4">Room's password</h4>
+            <h4 className="text-info mb-4">{text.rmspassword}</h4>
             <div className="input-group" onKeyPress={this.passwordKeyPressed}>
-                <Password className="form-control" placeholder="Enter password" value={this.state.password}
+                <Password className="form-control" placeholder={text.pswdplcholder} value={this.state.password}
                     onChange={this.passwordChanged} />
                 <div className="input-group-append">
                     <button className="btn btn-primary" onClick={this.confirmPassword}>
